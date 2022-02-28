dc27b95fb79fec57ffa31e4533606bad1a555e42
ChanYangYu <goodtss@soongsil.ac.kr>
2022-03-01 00:26:56 +0900

    fix: linked-list 수정
    - delete 버그 수정
      - prev = cur 코드 추가
    - insert, delete 코드 파라미터 변경
    -

--- _linkedList.c
+++ _linkedList.c
@@ -11,50 +11,56 @@ LinkedList* make_node(int idx, int user_key){
   return tmp;
 }
 
-int insert_node(LinkedList **head, int user_key){
-  LinkedList *next, *tmp;
+int insert_node(LinkedList **head, MyState *state_buf){
+  LinkedList *next;
   LinkedList *cur = *head;
 
   if(*head == NULL){
-    *head = make_node(1, user_key);
-    return;
+    *head = make_node(1, state_buf->user_key);
+    state_buf->unixtime = time(NULL);
+    return 0;
   }
 
   while(cur != NULL){
     next = cur->next;
-
     if(cur->idx == PARKING_LOT_SIZE)
       return -1;
 
     // 마지막 노드인 경우
-    if(next == NULL)
-      cur->next = make_node(cur->idx + 1, user_key);
+    if(next == NULL){
+      cur->next = make_node(cur->idx + 1, state_buf->user_key);
+      state_buf->unixtime = time(NULL);
+      return 0;
+    }
     // 중간에 비어있는 경우 
     else if(next->idx > cur->idx + 1){
-      cur->next = make_node(cur->idx + 1, user_key);
+      cur->next = make_node(cur->idx + 1, state_buf->user_key);
       cur->next->next = next;
+      state_buf->unixtime = time(NULL);
+      return 0;
     }
     
     cur = cur->next;
   }
-
-  return 0;
+  return -1;
 }
 
-int delete_node(LinkedList** head, int key){
+int delete_node(LinkedList** head, MyState *state_buf){
   LinkedList *prev = NULL;
   LinkedList *cur = *head;
 
   while(cur != NULL){
-    if(key == cur->key){
+    if(state_buf->user_key == cur->key){
       if(prev == NULL)
         *head = cur->next;
       else
         prev->next = cur->next;
       
+      state_buf->unixtime = cur->in_time;
       free(cur);
       return 0;
     }
+    prev = cur;
   }
   
   return -1;
--- _linkedList.h
+++ _linkedList.h
@@ -1,7 +1,9 @@
 #ifndef LINK_LIST_H
 #define LINK_LIST_H
 #define PARKING_LOT_SIZE 10
+#include <stdlib.h>
 #include <time.h>
+#include "_ipc.h"
 
 typedef struct LinkedList {
   int idx;
@@ -10,8 +12,7 @@ typedef struct LinkedList {
   struct LinkedList* next;
 }LinkedList;
 
-LinkedList* make_node(int idx, int user_key);
-int insert_node(LinkedList **head, int user_key);
-int delete_node(LinkedList** head, int key);
+int insert_node(LinkedList **head, MyState *state_buf);
+int delete_node(LinkedList **head, MyState *state_buf);
 
 #endif

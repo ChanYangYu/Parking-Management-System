173185520dd9a27fcdd7ea5e0314db6ada66453a
ChanYangYu <goodtss@soongsil.ac.kr>
2022-03-02 16:31:32 +0900

    feat: 출력 포맷 통일
    Co-authored-by: sung-hoon-kim <s_hun1996@naver.com>

--- manager.c
+++ manager.c
@@ -24,11 +24,13 @@ void current_parking_state(){
 
   if(manage_buf.errno == REQ_SUCCESS)
     printf("%s\n",manage_buf.response);
-  else
-    printf("Parking State Error!\n");
-  
+  else{
+    printf("-----------------------------------------------\n");
+    printf("[!] 주차현황 조회에 실패했습니다.\n");
+    printf("-----------------------------------------------\n");
+  }
   printf("정보를 조회하고 싶은 차량의 주차장 번호를 입력하세요: ");
-  scanf("%d", &manage_buf.pos);      // scanf로 이렇게 받아도 되는건지 질문
+  scanf("%d", &manage_buf.pos);
 
   manage_buf.msgtype = MSG_FIND_CARS_DETAIL_REQ;
   if(msgsnd(key, (void *)&manage_buf, sizeof(Manage) - sizeof(long), 0) == -1){
@@ -41,20 +43,23 @@ void current_parking_state(){
     exit(1);
   }
   
-  if(register_buf.errno == REQ_SUCCESS)      // 정보 조회 성공
-    printf("%s\n",register_buf.name);      // %s %s 로 쪼개서 표현 가능한지 질문
-  else                  // 정보 조회 실패
-    printf("Detail Info Error!\n");
+  system("clear");
+  printf("-----------------------------------------------\n");
+  if(register_buf.errno == REQ_SUCCESS)
+    printf("차주 이름 : %s\n차주 연락처 : %s\n차량번호 : %s\n",register_buf.name, register_buf.phone_number, register_buf.car_number);
+  else 
+    printf("[!] 해당 위치에는 주차된 차량이 없습니다.\n");
+  printf("-----------------------------------------------\n");
 }
 
 void parking_history(){
   int history_select_number;
   
-  printf("통합 이력 조회를 하려면 1번, 개인 이력 조회를 하려면 2번을 눌러주세요 : ");
+  printf("1.통합 이력 조회\n2.개인 이력 조회\n:");
   scanf("%d", &history_select_number);
 
-  if(history_select_number == 1){      // 통합 이력 조회
-    state_buf.msgtype = MSG_FIND_ALL_HISTORY_REQ;      // 메시지 큐 변수 확인하고 변경해야함
+  if(history_select_number == 1){ 
+    state_buf.msgtype = MSG_FIND_ALL_HISTORY_REQ;     
     if(msgsnd(key, (void *)&state_buf, sizeof(MyState) - sizeof(long), 0) == -1){
       fprintf(stderr,"Error: msgsnd() error\n");
       exit(1);
@@ -64,17 +69,24 @@ void parking_history(){
       fprintf(stderr,"Error: msgrcv() error\n");
       exit(1);
     }
-    
-    if(state_buf.errno == REQ_SUCCESS)
+    system("clear");
+    if(state_buf.errno == REQ_SUCCESS){
+      printf("-----------------------------------------------\n");
       printf("%s\n",manage_buf.response);
+      printf("-----------------------------------------------\n");
+    }
     else{
-      printf("Parking History Error!\n");
+      printf("-----------------------------------------------\n");
+      printf("[!] 통합 주차이력이 없습니다.\n");
+      printf("-----------------------------------------------\n");
     }
   }
   
   else if(history_select_number == 2){
-    
-    state_buf.msgtype = MSG_FIND_USER_HISTORY_REQ;      // 메시지 큐 변수 확인하고 변경해야함
+    printf("차량 번호를 입력해주세요 : ");
+    scanf("%s", state_buf.car_number);
+
+    state_buf.msgtype = MSG_FIND_USER_HISTORY_REQ;     
     if(msgsnd(key, (void *)&state_buf, sizeof(MyState) - sizeof(long), 0) == -1){
       fprintf(stderr,"Error: msgsnd() error\n");
       exit(1);
@@ -85,16 +97,24 @@ void parking_history(){
       exit(1);
     } 
     
-    if(manage_buf.errno == REQ_SUCCESS)
+    system("clear");
+    if(manage_buf.errno == REQ_SUCCESS){
+      printf("------------------------------------------------\n");
       printf("%s\n",manage_buf.response);
+      printf("------------------------------------------------\n");
+    }
     else{
-      printf("User History Error!\n");
-      exit(1);
+      printf("-----------------------------------------------\n");
+      printf("[!] %s 차량의 주차이력이 없습니다.\n", state_buf.car_number);
+      printf("-----------------------------------------------\n");
     }
   }
 
   else{
-    printf("잘못된 번호를 누르셨습니다.\n");
+    system("clear");
+    printf("-----------------------------------------------\n");
+    printf("[!] 잘못된 번호를 누르셨습니다.\n");
+    printf("-----------------------------------------------\n");
   }
 }
 
@@ -110,10 +130,17 @@ void user_list_all(){
     exit(1);
   }
 
-  if(manage_buf.errno == REQ_SUCCESS)
+  system("clear");
+  if(manage_buf.errno == REQ_SUCCESS){
+    printf("------------------------------------------------\n");
     printf("%s\n",manage_buf.response);
-  else
-    printf("[!] 조회할 유저가 없습니다.\n");
+    printf("------------------------------------------------\n");
+  }
+  else{
+    printf("------------------------------------------------\n");
+    printf("[!] 조회할 주차장 이용자가 없습니다.\n");
+    printf("------------------------------------------------\n");
+  }
 }
 
 void user_list_pen(){
@@ -128,19 +155,28 @@ void user_list_pen(){
     exit(1);
   }
 
-  if(manage_buf.errno == REQ_SUCCESS)
+  if(manage_buf.errno == REQ_SUCCESS){
+    printf("------------------------------------------------\n");
     printf("%s\n",manage_buf.response);
-  else
-    printf("[!] 조회할 유저가 없습니다.\n");
+    printf("------------------------------------------------\n");
+    
+  }
+  else{
+    printf("------------------------------------------------\n");
+    printf("[!] 예비 입주자가 없습니다.\n");
+    printf("------------------------------------------------\n");
+  }
+  
 }
 
 void update_user(){
-  
   user_list_pen();
   register_buf.msgtype = MSG_UPDATE_REQ;
   
-  printf("차량 번호와 입주자 여부(외부인 : 0 , 입주자 : 2)를 입력해주세요 :\n");
-  scanf("%s %d", register_buf.car_number, &register_buf.is_resident);
+  printf("차량 번호를 입력해주세요 : ");
+  scanf("%s", register_buf.car_number);
+  printf("입주자 인지 확인해주세요.\n(외부인 = 0 ,입주민 = 2)\n: ");
+  scanf("%d", &register_buf.is_resident);
   
   if(msgsnd(key, (void *)&register_buf, sizeof(Register) - sizeof(long), 0) == -1){
     fprintf(stderr,"Error: msgsnd() error\n");
@@ -152,10 +188,17 @@ void update_user(){
     exit(1);
   }
 
-  if(register_buf.errno == REQ_SUCCESS)
-    printf("Update Success\n");
-  else
-    printf("Update Fail\n");
+  system("clear");
+  if(register_buf.errno == REQ_SUCCESS){
+    printf("-----------------------------------------------\n");
+    printf("[!] 입주민 정보를 업데이트했습니다.\n");
+    printf("-----------------------------------------------\n");
+  }
+  else{
+    printf("-----------------------------------------------\n");
+    printf("[!] 업데이트에 실패했습니다.\n");
+    printf("-----------------------------------------------\n");
+  }
 }
 
 void find_name_number(){
@@ -174,10 +217,17 @@ void find_name_number(){
     exit(1);
   }
 
-  if(register_buf.errno == REQ_SUCCESS)
-    printf("해당 차량번호 %s의 차주는 %s 님 이고, 휴대폰 번호는 %s 입니다.\n", register_buf.car_number, register_buf.name, register_buf.phone_number);
-  else
-    printf("잘못된 번호를 입력하셨거나, 없는 사용자입니다.\n");
+  if(register_buf.errno == REQ_SUCCESS){
+    printf("-----------------------------------------------\n");
+    printf("차량번호 : %s\n차주 : %s\n휴대폰 번호 : %s\n", register_buf.car_number, register_buf.name, register_buf.phone_number);
+    printf("-----------------------------------------------\n");
+  }
+    
+  else{
+    printf("-----------------------------------------------\n");
+    printf("[!] 잘못된 번호를 입력하셨거나, 없는 사용자입니다.\n");
+    printf("-----------------------------------------------\n");    
+  }
 }
 
 
@@ -232,7 +282,8 @@ int main()
 
   while(1){
     int button;
-    printf("1. 주차현황\n2. 이력조회\n3. 업데이트\n4. 유저 전체조회\n5. 입주민 보류자 조회\n6. 차주 및 연락처 조회\n: ");
+    system("cat manager_title");
+    printf("1. 주차현황 조회\n2. 주차이력 조회\n3. 입주민 업데이트\n4. 이용자 전체조회\n5. 입주민 보류자 조회\n6. 차주 및 연락처 조회\n: ");
     scanf("%d", &button);
 
     system("clear");
